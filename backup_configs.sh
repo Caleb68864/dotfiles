ad576cf964885bfb9b94ba0b3cc0e71b5c614e39
Caleb Bennett <i3@arch>
2017-10-31 12:04:57 -0500

    Updated TMUX to use default session

---
+++ backup_configs.sh
@@ -0,0 +1,29 @@
+mkdir -p ~/github/dotfiles/i3_config
+mkdir -p ~/github/dotfiles/i3_config/i3blocks
+mkdir -p ~/github/dotfiles/openbox_config
+mkdir -p ~/github/dotfiles/tint2_config
+mkdir -p ~/github/dotfiles/polybar_config
+mkdir -p ~/github/dotfiles/fonts
+mkdir -p ~/github/dotfiles/vim
+
+cp -r ~/.config/i3/* ~/github/dotfiles/i3_config
+cp -r /usr/lib/i3blocks/* ~/github/dotfiles/i3_config/i3blocks
+cp -r /etc/i3status.conf ~/github/dotfiles/i3_config
+cp -r ~/.config/openbox/* ~/github/dotfiles/openbox_config
+cp -r ~/.config/tint2/* ~/github/dotfiles/tint2_config
+cp -r ~/.config/polybar/* ~/github/dotfiles/polybar_config
+cp ~/.config/compton.conf ~/github/dotfiles/
+cp -r ~/.fonts/* ~/github/dotfiles/fonts
+cp ~/.vimrc ~/github/dotfiles/vim
+cp -r ~/.vim ~/github/dotfiles/vim
+
+git config --global user.email "i3@arch"
+git config --global user.name "Caleb Bennett"
+
+
+git add -A
+DATE=$(date +"%Y-%m-%d %H:%M:%S")
+#echo $DATE
+git commit -m "$DATE"
+#git remote add origin https://github.com/Caleb68864/dotfiles.git
+git push origin master
---
+++ ohmyzsh_install.sh
@@ -0,0 +1,5 @@
+# Arch
+# sudo pacman -S zsh
+# Ubuntu/Debian
+sudo pacman -S zsh
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
