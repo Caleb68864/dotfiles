173de578064b21442a6e30a561e8e00e63476e66
Caleb Bennett <cbennett68864@gmail.com>
2019-01-07 15:46:32 +0000

    Fixed st install script and removed old st build file

--- install_scripts/apt/st_apt_install.py
+++ install_scripts/apt/st_apt_install.py
@@ -17,6 +17,7 @@ apps.append("libxft2-dev")
 run_cmd(" ".join(apps))
 
 #Clone st
-run_cmd("git clone --recursive https://github.com/LukeSmithxyz/st")
+run_cmd("git clone --recursive https://github.com/LukeSmithxyz/st st_build")
 run_cmd("make -C st")
 run_cmd("sudo make install -C st")
+run_cmd("sudo rm -R st_build)
