cf2fb0f557ed7e3e1078aa78264c14367c761644
Caleb Bennett <cbennett68864@gmail.com>
2019-03-22 11:13:38 -0500

    Updating gog_games script to launch ranger for directories

--- Scripts/gog_games.sh
+++ Scripts/gog_games.sh
@@ -1,10 +1,15 @@
 gog="~/GOG\ Games/"
 if [ -d "$gog" ]; then
-games=$(du --max-depth=1 $gog | awk '{for (i=2; i<NF; i++) print $i " " $NF}' | rofi -dmenu)
-echo $games
-command=$(du -a --max-depth=1 "$games" | awk '{for (i=2; i<NF; i++) print $i " " $NF}' | rofi -dmenu)
-#echo $command
-"$command"
+    games=$(du --max-depth=1 $gog | awk '{for (i=2; i<NF; i++) print $i " " $NF}' | rofi -dmenu)
+    echo $games
+    command=$(du -a --max-depth=1 "$games" | awk '{for (i=2; i<NF; i++) print $i " " $NF}' | rofi -dmenu)
+    #echo $command
+
+    if [ -d "$gog" ]; then
+        st -e ranger "$command"
+    else    
+        "$command"
+    fi
 else
-echo "$gog Does Not Exists"
+    echo "$gog Does Not Exists"
 fi
