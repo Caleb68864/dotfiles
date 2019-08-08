80e8d57e34838e08b63d74e6d691ce35c4829f9a
Homestead <Homestead>
2019-08-08 11:44:11 -0500

    Made more chagnes to setup scripts

--- install_scripts/Arch/arch_setup.sh
+++ install_scripts/Arch/arch_setup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 sudo pacman -Syy
 sudo pacman -Su
-sudo pacman -S acpi aria2 dmenu fakeroot firefox gcc git make markdown mpv networkmanager openssh python python-pip ranger rofi sudo tmux vim vlc wkhtmltopdf xorg xorg-server xorg-xinit youtube-dl zathura zsh
+sudo pacman -S acpi aria2 cronie dmenu fakeroot feh firefox fuse fuse-common fzf gcc git go htop make markdown mpv networkmanager openssh python python-pip ranger rofi sudo tigervnc tmux vim vlc vsftpd wkhtmltopdf xorg xorg-server xorg-xinit youtube-dl zathura zsh
 
-sudo pip install youtube-dl bs4 pandas numpy 
+sudo pip install bs4 numpy pandas youtube-dl
+
+~/dotfiles/install_scripts/Arch/yay_setup.sh
+
+yay -S xorgxrdp xrdp
 
 mkdir -p ~/.local/share
 mkdir -p ~/.temp/Commute
@@ -15,7 +19,14 @@ git clone --recurse-submodules https://github.com/Caleb68864/dotfiles
 
 ~/dotfiles/deploy
 
-sudo make clean install -C ~/dotfiles/st
-sudo make clean install -C ~/dotfiles/slock
-sudo make clean install -C ~/dotfiles/slstatus
-sudo make clean install -C ~/dotfiles/dwm
+~/dotfiles/install_scripts/suckless_setup.sh
+
+sudo systemctl start cronie
+sudo systemctl start NetworkManager
+sudo systemctl start sshd
+sudo systemctl start vsftpd
+
+sudo systemctl enable cronie
+sudo systemctl enable NetworkManager
+sudo systemctl enable sshd
+sudo systemctl enable vsftpd
