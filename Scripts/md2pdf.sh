668f32fef2ac5c9725f0455b281b307152c17c3a
Caleb <Caleb>
2019-09-03 14:23:09 -0500

    More mods to md2pdf

--- Scripts/md2pdf.sh
+++ Scripts/md2pdf.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 file="$1"
+sudo chmod 777 -R "$file"
 if [ -f "$file" ] && [ -n "$file" ]; then
     name="${file%%.*}"
     #pandoc --metadata title="$name" -T "$name"  -s "$file" -o "$name.html"  
     #markdown -b $file -f links,image,html,header > "$name.html"
+    #sudo chmod 777 -R "$name.html"
     markdown "$file" > "$name.html"
     if [ -f "$name.html" ]; then
         wkhtmltopdf --title "$name" --keep-relative-links -B 0 -L 0 -R 0 -T 0 -q "$name.html" "$name.pdf" 
-        #rm "$name.html"   
+        rm "$name.html"   
     fi
     #sudo chmod 777 -R *
 else
