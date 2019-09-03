33d96ff14b3af6dee652bb886d5c128d6df80529
Homestead <Homestead>
2019-09-03 14:05:40 -0500

    Modded md2pdf

--- Scripts/md2pdf_markdown.sh
+++ Scripts/md2pdf_markdown.sh
@@ -4,10 +4,10 @@ if [ -f "$file" ] && [ -n "$file" ]; then
     name="${file%%.*}"
     #pandoc --metadata title="$name" -T "$name"  -s "$file" -o "$name.html"  
     #markdown -b $file -f links,image,html,header > "$name.html"
-    markdown "$file" > "$name.html"
+    markdown $file > "$name.html"
     if [ -f "$name.html" ]; then
-        wkhtmltopdf --title "$name" --keep-relative-links -B 0 -L 0 -R 0 -T 0 -q "$name.html" "$name.pdf" 
-        #rm "$name.html"   
+        wkhtmltopdf --title "$name" --keep-relative-links -q "$name.html" "$name.pdf" 
+        rm "$name.html"   
     fi
     #sudo chmod 777 -R *
 else
