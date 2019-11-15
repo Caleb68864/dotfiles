fc1061388240ef44903c09a6e983862cadcc99b6
caleb <caleb@local>
2019-11-15 10:21:44 -0600

    Added keyboard shortcuts to sxiv and updated camera_card script with name option

--- Scripts/camera_card.sh
+++ Scripts/camera_card.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-device=$(sudo blkid | awk '{print substr($1, 0, length($1) - 1)}' | fzf)
+device=$(blkid | awk '{print substr($1, 0, length($1) - 1)}' | fzf)
 
 #echo $device
 
 if [ ! -z "$device" ]
 then
-    d=$(date +%F_%H%M)
+    confirm=false
+    while [ $confirm == false ]
+    do
+        read -p "Enter Camera Name: " name
+        read -p "Is $name Correct? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || confirm=false
+    done
+    d=$(date +%F_%H%M)_$name
     year=$(date +%Y)
 
     temp_dir=${d}_temp
@@ -26,7 +32,14 @@ then
     fi
 
     ~/dotfiles/Scripts/extract_pics.sh $temp_dir $d
-    rclone copy $d "GDrive:Game Cam/$year/$d" -P
+    
+    if [ ! -d $year ]
+    then
+        mkdir $year
+    fi
+
+    mv $d $year
+    #rclone copy --max-age 24h --no-traverse -P $year "GDrive:Game Cam/$year" 
 
     sudo umount $temp_dir
     rm -r $temp_dir
