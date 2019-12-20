be42c6caff84bc7838a5c48b672530e7ea536f7e
caleb <caleb@local>
2019-12-20 09:17:35 -0600

    Updated rofi config

---
+++ rofi/scripts/appsmenu.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+rofi -no-lazy-grab -modi drun -show drun -theme ../themes/appsmenu.rasi
+
---
+++ rofi/scripts/powermenu.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+rofi_command="rofi -theme ../themes/powermenu.rasi"
+
+### Options ###
+power_off=""
+reboot=""
+lock=""
+suspend="⏾"
+log_out=""
+# Variable passed to rofi
+options="$power_off\n$reboot\n$lock\n$suspend\n$log_out"
+
+chosen="$(echo -e "$options" | $rofi_command -dmenu -selected-row 2)"
+case $chosen in
+    $power_off)
+        systemctl poweroff
+        ;;
+    $reboot)
+        systemctl reboot
+        ;;
+    $lock)
+        #light-locker-command -l
+        slock
+        ;;
+    $suspend)
+        mpc -q pause
+        amixer set Master mute
+        systemctl suspend
+        ;;
+    $log_out)
+        #i3-msg exit
+        killall dwm
+        ;;
+esac
+
