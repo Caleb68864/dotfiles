3081e3dbac1bcaf14a11f68f67ebe1f59813469d
Caleb Bennett <serenityvalley06@gmail.com>
2020-06-10 13:03:20 -0500

    Update dwm.c

--- dwm/dwm.c
+++ dwm/dwm.c
@@ -135,6 +135,31 @@ struct Monitor {
 	const Layout *lt[2];
 };
 
+struct Monitor {
+	char ltsymbol[16];
+	float mfact;
+	int nmaster;
+	int num;
+	int by;               /* bar geometry */
+	int mx, my, mw, mh;   /* screen size */
+	int wx, wy, ww, wh;   /* window area  */
+	int gappih;           /* horizontal gap between windows */
+	int gappiv;           /* vertical gap between windows */
+	int gappoh;           /* horizontal outer gaps */
+	int gappov;           /* vertical outer gaps */
+	unsigned int seltags;
+	unsigned int sellt;
+	unsigned int tagset[2];
+	int showbar;
+	int topbar;
+	Client *clients;
+	Client *sel;
+	Client *stack;
+	Monitor *next;
+	Window barwin;
+	const Layout *lt[2];
+};
+
 typedef struct {
 	const char *class;
 	const char *instance;
